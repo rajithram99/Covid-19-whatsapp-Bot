e804d4fa657e8de60785c3950f283909fa5b5f73
Rajith <65568794+rajithram99@users.noreply.github.com>
2020-06-27 16:19:55 +0530

    Delete Scheduler.py

--- Scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from script import display_all
-
-sched = BlockingScheduler()
-
-sched.add_job(display_all, 'cron', seconds=5)
-sched.start()
